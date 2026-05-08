07706559ae7882946acc70eca8fe1b0e914c715d
Typed SIGTERM <typed.sigterm@gmail.com>
2026-05-08 21:32:05 +0800

    fix: camera

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,6 +2,21 @@
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .setup(#[allow(unused_variables)] |app| {
+            #[cfg(target_os = "linux")]
+            {
+                use webkit2gtk::{WebViewExt, PermissionRequestExt};                
+                let window = app.get_webview_window("main").unwrap();
+                window.with_webview(|webview| {
+                    let wk_webview = webview.inner();
+                    wk_webview.connect_permission_request(|_, request| {
+                        request.allow();
+                        true
+                    });
+                }).unwrap();
+            }
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
